c5753a933d5222047165792cd7adee7778f64dfa
taeuk-works <taeuk.works@gmail.com>
2023-11-04 23:16:37 +0900

    크래쉬 모니터링 툴 추가

--- lib/module/agentapp/agent_main.dart
+++ lib/module/agentapp/agent_main.dart
@@ -1,14 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:photoapp/preference.dart';
-import 'package:photoapp/service/department_service.dart';
-import 'package:photoapp/service/user_service.dart';
-import 'package:photoapp/service/shop_service.dart';
 
 import 'agent_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initPreferences();
+  await Future.wait([
+    initPreferences(),
+    Firebase.initializeApp(),
+  ]);
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
 
   runApp(AgentApp());
 }
