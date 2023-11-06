ecdd8d0dc658fe4b35b44a01bd1026053f2c3dc5
taeuk-works <taeuk.works@gmail.com>
2023-11-06 17:04:28 +0900

    에이전트 FirebaseCrashlytics 제거

--- lib/module/agentapp/agent_main.dart
+++ lib/module/agentapp/agent_main.dart
@@ -1,6 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:photoapp/preference.dart';
 
@@ -10,17 +7,17 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Future.wait([
     initPreferences(),
-    Firebase.initializeApp(),
+    // Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform),
   ]);
 
-  FlutterError.onError = (errorDetails) {
-    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
-  };
-  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
-  PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
+  // FlutterError.onError = (errorDetails) {
+  //   FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  // };
+  // // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  // PlatformDispatcher.instance.onError = (error, stack) {
+  //   FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+  //   return true;
+  // };
 
 
   runApp(AgentApp());
